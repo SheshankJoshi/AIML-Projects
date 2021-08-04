e3239d771814414ded86cf139ea7ce1bbbf3a38e
SheshankJoshi <54117488+SheshankJoshi@users.noreply.github.com>
2021-08-05 00:08:58 +0530

    Testing_deployment_Flask

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+app = Flask(__name__)
 
-app=Flask(__name__)
-
-@app.route("/")
-def Entry_point():
-    print("This is a test app, that is deployed on Heroku by Sheshank Joshi\n")
-    print("This is just for testing purpose only")
-    print("Very soon Links will be added here for all the data that I have used")
-    return 
+@app.route('/')
+def home():
+   return render_template('home.html')
+if __name__ == '__main__':
+   app.run()
